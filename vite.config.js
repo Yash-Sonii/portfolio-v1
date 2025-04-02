711ee56851bf992cdf0b9dae8b3dabcf94af4c73
Yash Soni <113409179+Yash-Sonii@users.noreply.github.com>
2025-04-02 11:58:11 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/portfolio-v1/',  // 👈 Add this line (repo name)
-  build: {
-    outDir: 'dist',   // Ensure correct output folder
-  },
+  base: '/portfolio-v1/', // Set this to your repository name
 });
